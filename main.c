c0d4e609914faae4bec9d8d9de6f34e04b226b20
Erin Turley <eturley@nd.edu>
2017-04-02 13:41:03 -0400

    added 1 line of code

--- main.c
+++ main.c
@@ -15,56 +15,55 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
-void page_fault_handler( struct page_table *pt, int page )
-{
-	printf("page fault on page #%d\n",page);
-	exit(1);
+void page_fault_handler( struct page_table *pt, int page ){
+  printf("page fault on page #%d\n",page);
+  exit(1);
 }
 
-int main( int argc, char *argv[] )
-{
-	if(argc!=5) {
-		printf("use: virtmem <npages> <nframes> <rand|fifo|lru|custom> <sort|scan|focus>\n");
-		return 1;
-	}
-
-	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
-	const char *program = argv[4];
-
-	struct disk *disk = disk_open("myvirtualdisk",npages);
-	if(!disk) {
-		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
-		return 1;
-	}
-
-
-	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
-	if(!pt) {
-		fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
-		return 1;
-	}
-
-	char *virtmem = page_table_get_virtmem(pt);
-
-	char *physmem = page_table_get_physmem(pt);
-
-	if(!strcmp(program,"sort")) {
-		sort_program(virtmem,npages*PAGE_SIZE);
-
-	} else if(!strcmp(program,"scan")) {
-		scan_program(virtmem,npages*PAGE_SIZE);
-
-	} else if(!strcmp(program,"focus")) {
-		focus_program(virtmem,npages*PAGE_SIZE);
-
-	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
-		return 1;
-	}
-
-	page_table_delete(pt);
-	disk_close(disk);
-
-	return 0;
+int main( int argc, char *argv[] ){
+  if(argc!=5) {
+    printf("use: virtmem <npages> <nframes> <rand|fifo|lru|custom> <sort|scan|focus>\n");
+    return 1;
+  }
+  
+  int npages = atoi(argv[1]);
+  int nframes = atoi(argv[2]);
+  const char *algorithm = argv[3];
+  const char *program = argv[4];
+  
+  struct disk *disk = disk_open("myvirtualdisk",npages);
+  if(!disk) {
+    fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
+    return 1;
+  }
+  
+  
+  struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
+  if(!pt) {
+    fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
+    return 1;
+  }
+  
+  char *virtmem = page_table_get_virtmem(pt);
+  
+  char *physmem = page_table_get_physmem(pt);
+  
+  if(!strcmp(program,"sort")) {
+    sort_program(virtmem,npages*PAGE_SIZE);
+    
+  } else if(!strcmp(program,"scan")) {
+    scan_program(virtmem,npages*PAGE_SIZE);
+    
+  } else if(!strcmp(program,"focus")) {
+    focus_program(virtmem,npages*PAGE_SIZE);
+    
+  } else {
+    fprintf(stderr,"unknown program: %s\n",argv[3]);
+    return 1;
+  }
+  
+  page_table_delete(pt);
+  disk_close(disk);
+  
+  return 0;
 }
